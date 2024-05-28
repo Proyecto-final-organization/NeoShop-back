18c6159d719c7a9ade4acd6868435fce9239e3ef
FacuMorales <facu44515119@gmail.com>
2024-05-28 19:21:17 -0300

    Armado basico de getUser

--- src/controllers/userControllers/getUser.js
+++ src/controllers/userControllers/getUser.js
@@ -1,7 +1,12 @@
 const { User } = require('../../db.js');
 
 const getUser = async (req, res) => {
-
+    try {
+        const users = await User.findAll({});
+        return res.status(200).json(users);
+    } catch (error) {
+        return res.status(500).json({error: error.message});
+    }
 };
 
 module.exports = getUser;
