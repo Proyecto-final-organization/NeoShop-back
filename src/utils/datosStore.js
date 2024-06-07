d491fc201e00ed178004ea24154220d0ab2d3464
MichaelKMedina <maicollagos@gmail.com>
2024-06-06 23:53:28 -0300

    arreglos de las tiendas

--- src/utils/datosStore.js
+++ src/utils/datosStore.js
@@ -3,12 +3,42 @@ module.exports = [
           "address_cp": "12345",
           "address_country": "Argentina",
           "address_city": "Buenos Aires",
-          "name": "Ferreteria Lopez"
+          "name": "Ferreteria Lopez",
+          "logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReWtdna4NlKIKF08Y50ejW_B6LSexwXHLtrQ&s"
         },
         {
           "address_cp": "54321",
           "address_country": "Argentina",
           "address_city": "Córdoba",
-          "name": "Ferreteria Martinez"
-        }
+          "name": "Ferreteria Martinez",
+          "logo": "https://st2.depositphotos.com/4335005/7939/v/450/depositphotos_79399298-stock-illustration-vector-flat-icon.jpg"
+        },
+        {
+        "address_cp": "54321",
+        "address_country": "Argentina",
+        "address_city": "Córdoba",
+        "name": "Falavella",
+        "logo": "https://images.falabella.com/v3/assets/blt7c5c2f2f888a7cc3/blt09274da39e1b6410/622b895af26a4b66ea4a1452/iconos-CDA-Horarios-Falabella-V1.png"
+      },
+      {
+        "address_cp": "54321",
+        "address_country": "Argentina",
+        "address_city": "Córdoba",
+        "name": "Musimundo",
+        "logo": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS0i9pRS28qXlxeUdrx2Rd16ByN5hHUvnpirw&s"
+      },
+      {
+        "address_cp": "54322",
+        "address_country": "Argentina",
+        "address_city": "Misiones",
+        "name": "Fravega",
+        "logo": "https://assets.hotsale.com.ar/uploads/brands/14464/orig-1713499149-fravega.jpg"
+      },
+      {
+        "address_cp": "54311",
+        "address_country": "Argentina",
+        "address_city": "Salta",
+        "name":"CompraGamer",
+        "logo": "https://i.ibb.co/wzwdDbs/313413301-552706986663673-2669822241094639465-n.jpg"
+      }
 ];
