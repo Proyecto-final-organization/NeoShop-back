83085be954c07f019e0d699acf638f37f3c7a439
Michael Medina Lagos <122126112+MichaelKMedina@users.noreply.github.com>
2024-06-13 23:06:01 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -5,12 +5,19 @@ const server = express();
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const invalidRoute = require("./middleware/invalidRoute");
+const corsOptions = {
+  origin: [
+    "https://neo-shop-front.vercel.app",
+    "https://neo-shop-dashboard-ngyjmqsrx-neoshopmarketplace.vercel.app"
+  ],
+  credentials: true // Habilita el envío de cookies
+};
 
 server.name = "API";
 server.use(morgan("dev"));
 server.use(express.json());
 server.use(cookieParser());
-server.use(cors({ origin: "*" }));
+server.use(cors(corsOptions));
 server.use(router);
 server.use(invalidRoute);
 
