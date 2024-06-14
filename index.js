be17853edee834cbc81e7347b4c135183710acfc
Michael Medina Lagos <122126112+MichaelKMedina@users.noreply.github.com>
2024-06-13 22:05:32 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { conn } = require("./src/db.js");
 const montarUsers = require("./src/utils/montarUsers.js");
 
 conn
-  .sync({ alter: true })//cambiar a force para trabajar localmente, alter el otro
+  .sync({ force: true })//cambiar a force para trabajar localmente, alter el otro
   .then(() => {
     server.listen(port, async () => {
       console.log(`Server listening on port ${port}`);
