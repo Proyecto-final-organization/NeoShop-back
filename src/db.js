e4804eb32194921f7b96a4089a8b73171747b8f6
Michael Medina Lagos <122126112+MichaelKMedina@users.noreply.github.com>
2024-06-13 21:56:21 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD,DB_HOST, DB_PORT, DB_NAME  } = process.env;
+const { DB_DEPLOY } = process.env;
 const userModel = require("./models/User");
 const productModel = require("./models/Product");
 const orderModel = require("./models/Order");
