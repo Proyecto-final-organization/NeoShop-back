355fa14d531832cf55d621b05636060205188d33
FacuMorales <facu44515119@gmail.com>
2024-06-18 12:15:44 -0300

    solucion getProductById

--- src/controllers/productControllers/getProductById.js
+++ src/controllers/productControllers/getProductById.js
@@ -2,38 +2,43 @@ const { product, category, store, brand, review, user } = require("../../db.js")
 const averageProductRating = require("../reviewControllers/averageProductRating.js");
 
 const getProductById = async (idProduct) => {
-  try {
-    const productSearch = await product.findByPk(idProduct, {
-      include: [
-        {
-          model: category,
-          through: { attributes: [] },
-          attributes: ["name"],
-        },
-        {
-          model: store,
-          attributes: ["name"],
-        },
-        {
-          model: brand,
-          attributes: ["name"],
-        },
-        {
-          model: review,
-          attributes: ["comment", "rating", "date"], // Incluir la fecha de la review
-          include: [
-            {
-              model: user,
-              attributes: ["name"], // Incluir el nombre del usuario que generó la review
-            },
-          ],
-        },
-      ],
-    });
+  const productSearch = await product.findByPk(idProduct, {
+    include: [
+      {
+        model: category,
+        through: { attributes: [] },
+        attributes: ["name"],
+      },
+      {
+        model: store,
+        attributes: ["name"],
+      },
+      {
+        model: brand,
+        attributes: ["name"],
+      },
+      {
+        model: review,
+        attributes: ["comment", "rating", "date"], // Incluir la fecha de la review
+        include: [
+          {
+            model: user,
+            attributes: ["name"], // Incluir el nombre del usuario que generó la review
+          },
+        ],
+      },
+    ],
+  });
 
   if (productSearch === null) {
     throw new Error("the product was not found on the database");
   }
+
+  // Calculate average rating
+  const averageRating = await averageProductRating(idProduct);
+
+  // Attach average rating to the product object
+  productSearch.average_mark = averageRating;
 
   // Formatear los datos
   const formattedProduct = {
