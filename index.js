a05e3a011e1d20570700e239999637540251d570
Michael Medina Lagos <122126112+MichaelKMedina@users.noreply.github.com>
2024-06-26 21:34:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
 require("dotenv").config();
 const port = process.env.PORT || 3001;
-const { httpServer } = require("./src/app.js");
-const sendReminderCart = require("./src/controllers/cartControllers/sendReminderCart.js");
+const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const montarUsers = require("./src/utils/montarUsers.js");
 
-// Configurar la ejecución cada 2 minutos (2 * 60 * 1000 milisegundos) para pruebas
-setInterval(sendReminderCart, 60 * 60 * 1000); 
-
 conn
-  .sync({ alter: true }) // Cambiar a force para trabajar localmente, alter el otro
-  .then(() => {
-    httpServer.listen(port, async () => {
+  .sync({ force: true })//cambiar a force para trabajar localmente, alter el otro
+  .then( () => {
+    server.listen(port, async () => {
       console.log(`Server listening on port ${port}`);
-      //await montarUsers();
     });
+    await montarUsers();
   })
-  .catch((error) => console.error("Database connection error:", error));
+  .catch((error) =>
+    console.error("Database connection error:", error)
+  );
 
