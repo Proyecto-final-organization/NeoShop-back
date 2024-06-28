05fd1eb6ea741acdcae51cce54b30e6714fdb5de
Michael Medina Lagos <122126112+MichaelKMedina@users.noreply.github.com>
2024-06-28 19:16:36 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -11,8 +11,14 @@ const whitelist = ["https://neo-shop-front.vercel.app/",
                    "http://localhost:5173/", 
                    "http://localhost:3000/"];
 
-const corsOptions ={
-  origin: whitelist, // Permitir todas las solicitudes
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   credentials: true, // Habilita el envío de cookies
 };
 
