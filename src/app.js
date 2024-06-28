83a6e1388516e87bb8cc3a7fef35979effd4bc0f
Michael Medina Lagos <122126112+MichaelKMedina@users.noreply.github.com>
2024-06-28 19:20:12 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,30 +6,39 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const invalidRoute = require("./middleware/invalidRoute");
 
-const whitelist = ["https://neo-shop-front.vercel.app/", 
-                   "https://neo-shop-dashboard-ngyjmqsrx-neoshopmarketplace.vercel.app/", 
-                   "http://localhost:5173/", 
-                   "http://localhost:3000/"];
+const whitelist = [
+  "https://neo-shop-front.vercel.app",
+  "neo-shop-dashboard-neoshopmarketplace.vercel.app",
+  "http://localhost:5173",
+  "http://localhost:3000"
+];
 
 const corsOptions = {
   origin: (origin, callback) => {
+    // Permitir todos los orígenes
+    callback(null, true);
+  },
+  credentials: (origin, callback) => {
+    // Habilitar el envío de cookies solo para los orígenes en la lista blanca
     if (whitelist.indexOf(origin) !== -1 || !origin) {
       callback(null, true);
     } else {
-      callback(new Error("Not allowed by CORS"));
+      callback(null, false);
     }
-  },
-  credentials: true, // Habilita el envío de cookies
+  }
 };
 
 server.name = "API";
 server.use(morgan("dev"));
 server.use(express.json());
 server.use(cookieParser());
-server.use(cors(corsOptions));
+server.use((req, res, next) => {
+  cors(corsOptions)(req, res, next);
+});
 server.use(router);
 server.use(invalidRoute);
 
 module.exports = server;
+
 
 
