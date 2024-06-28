b85cfc3a55705f45ac5ac1f741825926030fece4
Michael Medina Lagos <122126112+MichaelKMedina@users.noreply.github.com>
2024-06-28 19:11:41 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,15 +6,13 @@ const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const invalidRoute = require("./middleware/invalidRoute");
 
-const whitelist = [
-  "https://neo-shop-front-l7621ntd5-neoshopmarketplace.vercel.app",
-  "https://neo-shop-dashboard-loha0llqe-neoshopmarketplace.vercel.app",
-  "http://localhost:5173",
-  "http://localhost:3000"
-];
+const whitelist = ["https://neo-shop-front.vercel.app/", 
+                   "https://neo-shop-dashboard-ngyjmqsrx-neoshopmarketplace.vercel.app/", 
+                   "http://localhost:5173/", 
+                   "http://localhost:3000/"];
 
 const corsOptions ={
-  origin: '*', // Permitir todas las solicitudes
+  origin: whitelist, // Permitir todas las solicitudes
   credentials: true, // Habilita el envío de cookies
 };
 
