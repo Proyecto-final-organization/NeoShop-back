a4692a3af45f9e4b3c3a8a774cc398ad27fca794
Michael Medina Lagos <122126112+MichaelKMedina@users.noreply.github.com>
2024-06-28 19:05:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const montarUsers = require("./src/utils/montarUsers.js");
 
 conn
   .sync({ force: true })//cambiar a force para trabajar localmente, alter el otro
-  .then(() => {
-    server.listen(port, async () => {
+  .then(async () => {
+    server.listen(port, () => {
       console.log(`Server listening on port ${port}`);
     });
     await montarUsers();
