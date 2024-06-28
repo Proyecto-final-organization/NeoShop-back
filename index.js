40be63d8fd87a0b38ac50d344616587db74a2c16
Michael Medina Lagos <122126112+MichaelKMedina@users.noreply.github.com>
2024-06-28 19:51:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const montarUsers = require("./src/utils/montarUsers.js");
 
 
 conn
-  .sync({ force: true })//cambiar a force para trabajar localmente, alter el otro
-  .then(async () => {
+  .sync({ alter: true })//cambiar a force para trabajar localmente, alter el otro
+  .then(() => {
     server.listen(port, () => {
       console.log(`Server listening on port ${port}`);
     });
-    await montarUsers();
+    //await montarUsers();
     //setInterval(sendReminderCart, 2 * 60 * 1000);
   })
   .catch((error) =>
