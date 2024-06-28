a947f66d89653651ed444875b641a8485de1be94
Julian Gabriel Niveyro <115017654+NiveJulian@users.noreply.github.com>
2024-06-28 18:22:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ const { conn } = require("./src/db.js");
 const montarUsers = require("./src/utils/montarUsers.js");
 
 conn
-  .sync({ alter: true })//cambiar a force para trabajar localmente, alter el otro
+  .sync({ force: true })//cambiar a force para trabajar localmente, alter el otro
   .then(() => {
     server.listen(port, async () => {
       console.log(`Server listening on port ${port}`);
     });
-    //await montarUsers();
+    await montarUsers();
   })
   .catch((error) =>
     console.error("Database connection error:", error)
