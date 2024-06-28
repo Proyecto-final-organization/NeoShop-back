b2d863a316f719a0f874c95fe5b0689d3883ff84
Michael Medina Lagos <122126112+MichaelKMedina@users.noreply.github.com>
2024-06-28 19:02:45 -0300

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 const montarUsers = require("./src/utils/montarUsers.js");
 
+
+
 conn
   .sync({ force: true })//cambiar a force para trabajar localmente, alter el otro
   .then(() => {
@@ -11,6 +13,7 @@ conn
       console.log(`Server listening on port ${port}`);
     });
     await montarUsers();
+    //setInterval(sendReminderCart, 2 * 60 * 1000);
   })
   .catch((error) =>
     console.error("Database connection error:", error)
